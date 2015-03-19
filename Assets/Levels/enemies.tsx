543abd3186f8b45768b510d4308a72485e859340
Peter Clotworthy <clotters@gmail.com>
2015-03-19 19:43:31 +0000

    Minor XML changes, tilesets seperated

---
+++ Assets/Levels/enemies.tsx
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="enemies" tilewidth="30" tileheight="30">
+ <tile id="0">
+  <properties>
+   <property name="id" value="admin_1"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/admin_1.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="id" value="firewall_1"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/firewall_1.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="id" value="watchdog_1"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/guarddog_1.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="id" value="watchdog_2"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/guarddog_2.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="id" value="watchdog_3"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/guarddog_3.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="id" value="sensor_1"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/sensor_1.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="id" value="sensor_2"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/sensor_2.png"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="id" value="sensor_3"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/sensor_3.png"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="id" value="sentinel_1"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/sentinel_1.png"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="id" value="sentinel_2"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/sentinel_2.png"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="id" value="sentinel_3"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/sentinel_3.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="id" value="warden_1"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/warden_1.png"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="id" value="warden_2"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/warden_2.png"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="id" value="warden_3"/>
+  </properties>
+  <image width="29" height="30" source="../../enemy_tiles/warden_3.png"/>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="id" value="watchman_1"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/watchman_1.png"/>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="id" value="watchman_2"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/watchman_2.png"/>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="id" value="watchman_3"/>
+  </properties>
+  <image width="30" height="30" source="../../enemy_tiles/watchman_3.png"/>
+ </tile>
+</tileset>
