1a3f17f9b8d7caed467792d6b42032dff63c534e
Peter Clotworthy <clotters@gmail.com>
2015-03-19 19:41:28 +0000

    Level templates

---
+++ Assets/Levels/template/map_features.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map_features" tilewidth="30" tileheight="30">
+ <tile id="0">
+  <properties>
+   <property name="id" value="credit"/>
+  </properties>
+  <image width="30" height="30" source="../../../map_features/credit.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="id" value="intel"/>
+  </properties>
+  <image width="30" height="30" source="../../../map_features/intel.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="id" value="spawnpoint"/>
+  </properties>
+  <image width="28" height="28" source="../../../map_features/spawnpoint.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="id" value="path"/>
+  </properties>
+  <image width="30" height="30" source="../../../map_features/title_moveable.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="id" value="wall"/>
+  </properties>
+  <image width="30" height="30" source="../../../map_features/wall.png"/>
+ </tile>
+</tileset>
