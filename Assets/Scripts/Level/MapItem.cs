a32555e17e72148978256220cb30329a640a0cba
Peter Clotworthy <clotters@gmail.com>
2016-02-29 20:49:49 +0000

    Made GridCollection (and the new GridPiece class) generic, for future use

--- Assets/Scripts/Level/MapItem.cs
+++ Assets/Scripts/Level/MapItem.cs
@@ -1,9 +1,15 @@
-﻿using System.Xml.Serialization;
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using Scripts.Action.Move;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 [XmlInclude(typeof(Pickup))]
 [XmlType(TypeName = "mapitem")]
-public class MapItem {
+public class MapItem : IGridTileIdentifier{
     [XmlAttribute("name")]
     public string name;
     [XmlAttribute("description")]
@@ -16,17 +22,45 @@ public class MapItem {
     public Vector2 gridPosition;
     [XmlIgnore]
     public Sprite sprite;
+    [XmlIgnore]
+    public int id;
 
-    public virtual MapItem Copy(MapItem mi) {
-        MapItem newMI = new MapItem {
-            name = mi.name,
-            description = mi.description,
-            string_id = mi.string_id,
-            gameobject = null,
-            gridPosition = new Vector2(),
-            sprite = null
-        };
+    public MapItem() {
+    }
 
-        return newMI;
+    public MapItem(MapItem mi) {
+        name = mi.name;
+        description = mi.description;
+        string_id = mi.string_id;
+        gameobject = null;
+        gridPosition = new Vector2();
+        sprite = mi.sprite;
+    }
+
+    protected static T Clone<T>(T source) {
+        if(!typeof(T).IsSerializable) {
+            throw new ArgumentException("The type must be serializable.", "source");
+        }
+
+        // Don't serialize a null object, simply return the default for that object
+        if(System.Object.ReferenceEquals(source, null)) {
+            return default(T);
+        }
+
+        IFormatter formatter = new BinaryFormatter();
+        Stream stream = new MemoryStream();
+        using(stream) {
+            formatter.Serialize(stream, source);
+            stream.Seek(0, SeekOrigin.Begin);
+            return (T)formatter.Deserialize(stream);
+        }
+    }
+
+    public int ID {
+        get
+        {
+            return this.id;
+        }
+        set;
     }
 }
