888ecf1152ed3b69c20ef7dcccb67a2bf480491d
Peter Clotworthy <clotters@gmail.com>
2016-02-29 13:36:06 +0000

    Added GridCollection

--- Assets/Scripts/Level/MapItem.cs
+++ Assets/Scripts/Level/MapItem.cs
@@ -11,7 +11,22 @@ public class MapItem {
     [XmlAttribute("string_id")]
     public string string_id;
     [XmlIgnore]
-    public Vector2 position;
+    public GameObject gameobject;
+    [XmlIgnore]
+    public Vector2 gridPosition;
     [XmlIgnore]
     public Sprite sprite;
+
+    public virtual MapItem Copy(MapItem mi) {
+        MapItem newMI = new MapItem {
+            name = mi.name,
+            description = mi.description,
+            string_id = mi.string_id,
+            gameobject = null,
+            gridPosition = new Vector2(),
+            sprite = null
+        };
+
+        return newMI;
+    }
 }
