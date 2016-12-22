a5440f0e8ff7a84f0103d21af0f6743b5231f8d7
Peter Clotworthy <clotters@gmail.com>
2016-12-22 02:07:18 +0100

    Removed out-of-date commented code & base structure for in-Unity preview

--- Assets/Scripts/Controllers/ActiveLevel.cs
+++ Assets/Scripts/Controllers/ActiveLevel.cs
@@ -24,47 +24,18 @@ namespace Controllers {
 	    public delegate void LevelLoadedHandler(GridGraph<MapItem> graph, Dictionary<string, Sprite> sprites);
 	    public event LevelLoadedHandler DataLoaded;
 
-	    private bool levelLoaded;
-	    private bool spritesLoaded;
-
-
-
-        // TODO: This should really be on the GridPiece prefab (and the prefab should probably be called SoftwareTool and have the relevant object attached).
-
 	    private void Awake() {
 			ReadData();
 	    }
 
 	    protected override void Start () {
-            // Using XML data, map sprites to appropriate entities
-            // MUST take place AFTER loading, but BEFORE grid injection
-//            LoadSprites();
-
-            // Generate level from geometry/entity data
-//            InjectGeometry();
-//            InjectEntities();
-			//ReadData();
 
         }
 
         // Update is called once per frame
         private void Update() {
-//        if (Input.GetMouseButtonDown(0)) {
-//            test?.Move(new Vector2(5, 7));
-//            Debug.Log(string.Format("Attacking: {0}, Source: {1}", entityGrid.Get(1, 15).Value as Sentry, test));
-//            //Debug.Log(test.Attacks);
-//            test?.Attacks[0]?.Execute((SoftwareTool)entityGrid.Get(1, 15).Value, test);
-//
-//            //Debug.Log(string.Format("New position of {0} is {1}", entityGrid.Get(test.GetPosition()).Value.name, entityGrid.Get(test.GetPosition()).Value.GetPosition()));
-//        }
-	        if (levelLoaded) {
-				throw new AbandonedMutexException();
-	        }
+
         }
-
-    
-
-    
 
         private void ReadData() {
 	        EntityModel.dumpEnemyXML = dumpEnemyXML;
